987009c5fb58a0d16d625ec88211ad6b58fbcdd3
SooryaBhat <sooryabhat49@gmail.com>
2026-06-06 19:52:46 +0530

    change api

--- attendiq/frontend/assets/js/api.js
+++ attendiq/frontend/assets/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://securely-masculine-elliptic.ngrok-free.dev";
 
 function getAuthToken() {
   return localStorage.getItem("attendiq_token");
