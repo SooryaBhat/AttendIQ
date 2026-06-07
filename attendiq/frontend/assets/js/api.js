ae4e08d65aeec56d8e1d34d9a03a01cb01f4e74c
SooryaBhat <sooryabhat49@gmail.com>
2026-06-07 20:50:25 +0530

    change link

--- attendiq/frontend/assets/js/api.js
+++ attendiq/frontend/assets/js/api.js
@@ -1,4 +1,4 @@
-const LOCAL_API_BASE_URL = "http://127.0.0.1:8000";
+const LOCAL_API_BASE_URL = "http://127.0.0.1:4040 ";
 const API_BASE_URL = window.location.protocol.startsWith("http")
   ? (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
       ? LOCAL_API_BASE_URL
